d9c6aae51d96c7385dcfce9ec6ec6a49207fd0cd
nam <colossalpen12@gmail.com>
2021-10-10 23:55:23 +0700

    adding dictionary and settings. Usable buttons;

--- src/main/java/tudienbachkhoa/dictionary/DictionaryApplication.java
+++ src/main/java/tudienbachkhoa/dictionary/DictionaryApplication.java
@@ -3,6 +3,7 @@ package tudienbachkhoa.dictionary;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,9 +13,9 @@ import java.io.IOException;
 public class DictionaryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DictionaryApplication.class.getResource("DictionaryView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DictionaryApplication.class.getResource("Menu.fxml"));
         stage.initStyle(StageStyle.TRANSPARENT);
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 600);
         scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Quan-Quy-Truong Dictionary");
         stage.setScene(scene);
--- src/main/java/tudienbachkhoa/dictionary/SceneController.java
+++ src/main/java/tudienbachkhoa/dictionary/SceneController.java
@@ -1,5 +1,52 @@
 package tudienbachkhoa.dictionary;
 
-public class SceneController {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    public void switchToSettings(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("OptionsView.fxml"));
+        scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToMainMenu(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void OpenDictionary(ActionEvent e) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("DictionaryView.fxml"));
+        scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void ExitApp(ActionEvent event) throws IOException{
+        Platform.exit();
+    }
 }
