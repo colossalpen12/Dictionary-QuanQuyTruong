a374d010310696f7af86357dacfa41587868a65a
Hotaru852 <91526646+Hotaru852@users.noreply.github.com>
2021-10-17 19:38:39 +0700

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,19 @@
+module tudienbachkhoa.dictionary {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+    requires java.sql;
+    requires com.jfoenix;
+    requires jsapi;
+
+    opens tudienbachkhoa.dictionary to javafx.fxml;
+    exports tudienbachkhoa.dictionary;
+}
